46cb251cd36bca5c42d50598bec7f62717516944
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2015-12-09 20:39:36 +0200

    upd

--- src/js/common.js
+++ src/js/common.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
 
-	// go js
-	$('.js-slider-instafeed').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false
-	});
+    // go js
+    $('.js-slider-instafeed').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+    });
+    // feed
+    var $json = 'https://api.instagram.com/v1/tags/snowy/media/recent?access_token=1828642866.1677ed0.79d9608c2a8a46db9db35304d9ce43fa&callback=?';
+
+    $.getJSON($json, function(el) {
+        $.each(el, function(i, key) {
+        	console.log(el);
+        })
+    });
 });
